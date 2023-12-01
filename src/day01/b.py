fe8fbc9e056e37b5227be4a7b1509a3b9420b8be
gulliver-madrid <49131885+gulliver-madrid@users.noreply.github.com>
2023-12-01 07:28:53 +0100

    1b: use functions

--- src/day01/b.py
+++ src/day01/b.py
@@ -10,50 +10,61 @@ def letter_to_int(letter: str) -> int:
     return letters.index(letter) + 1  # pyright: ignore
 
 
+def get_first(line: str) -> int:
+    first = None
+
+    for i, char in enumerate(line):
+        if char.isdigit():
+            first = int(char)
+            break
+        found = False
+        for let in letters:
+            if line.startswith(let, i):
+                first = letter_to_int(let)
+                found = True
+                break
+        if found:
+            break
+    else:
+        assert False, "No se encontró el primer dígito"
+    assert first is not None
+    return first
+
+
+def get_last(line: str) -> int:
+    last = None
+
+    n = len(line)
+    for i in range(n - 1, -1, -1):
+        char = line[i]
+        if char.isdigit():
+            last = int(char)
+            break
+
+        found = False
+        for let in letters:
+            if line.endswith(let, 0, i + 1):
+                last = letter_to_int(let)
+                found = True
+                break
+        if found:
+            break
+    else:
+        assert False, "No se encontró el último dígito"
+    assert last is not None
+    return last
+
+
+def line_to_calibration(line: str) -> int:
+    first = get_first(line)
+    last = get_last(line)
+    return first * 10 + last
+
+
 def solve(lines: Lines) -> int:
     total = 0
     for line in lines:
-        first = None
-        last = None
-
-        for i, char in enumerate(line):
-            if char.isdigit():
-                first = int(char)
-                break
-            found = False
-            for let in letters:
-                if line.startswith(let, i):
-                    first = letter_to_int(let)
-                    found = True
-                    break
-            if found:
-                break
-        else:
-            assert False, "No se encontró el primer dígito"
-
-        n = len(line)
-        for i in range(n - 1, -1, -1):
-            char = line[i]
-            if char.isdigit():
-                last = int(char)
-                break
-
-            found = False
-            for let in letters:
-                if line.endswith(let, 0, i + 1):
-                    last = letter_to_int(let)
-                    found = True
-                    break
-            if found:
-                break
-        else:
-            assert False, "No se encontró el último dígito"
-
-        assert first is not None
-        assert last is not None
-        calibration = first * 10 + last
-        total += calibration
-
+        total += line_to_calibration(line)
     return total
 
 
